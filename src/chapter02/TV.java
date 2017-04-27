ddef35ffe02915504e1d47c913c7456936246d66
kkww <kkww@kkww>
2017-04-27 13:19:42 +0900

    a

---
+++ src/chapter02/TV.java
@@ -0,0 +1,17 @@
+package chapter02;
+
+public class TV {
+	private int channel;
+	private int volume;
+	private boolean power;
+	
+	public int getChannel() {
+		return channel;
+	}
+	public int getVolume() {
+		return volume;
+	}
+	public boolean isPower() {
+		return power;
+	}
+}
